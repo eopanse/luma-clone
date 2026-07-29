03670dbfc57dba66f53bcd36a26f3514c8e1d34b
eopanse <eopanse2013@gmail.com>
2026-07-29 14:32:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log('Hello my name is Munachi')
+
+let firstName = 'Munachi'
+const age = 13
+
+console.log(firstName);
+console.log(age);
+
+let firstNumber = 10;
+let secondNumber = 768;
+
+console.log(firstNumber / secondNumber);
+
+prompt('how old are you');
+alert('Welcome');
